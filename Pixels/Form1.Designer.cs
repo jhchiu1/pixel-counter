b102073592c48b4920e0f3ba91d54ba5e8b95414
bh3618gd <bh3618gd@T301016-1625331>
2016-10-18 20:41:38 -0500

    Change form background color

--- Pixels/Form1.Designer.cs
+++ Pixels/Form1.Designer.cs
@@ -44,6 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aquamarine;
             this.ClientSize = new System.Drawing.Size(663, 600);
             this.Controls.Add(this.pixelsLabel);
             this.Name = "Form1";
